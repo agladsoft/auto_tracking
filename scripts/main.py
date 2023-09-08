ca9ee6b5e9ba5c861a8e611e1ecd6fe6fe117f28
timur <timurka.zavyalov@mail.ru>
2023-09-08 10:42:51 +0300

    git init

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import pandas as pd
+from typing import Tuple
 from pandas import DataFrame
+from pandas.core.groupby import DataFrameGroupBy
 
 
 class AutoTracking(object):
@@ -10,16 +12,26 @@ class AutoTracking(object):
 
     @staticmethod
     def write_rows_by_terminal(df: DataFrame, value: str, path: str) -> None:
+        """
+        We group by 'enforce_auto_tracking', 'year', 'month' and write it to a separate xlsx file.
+        :param df: DataFrame.
+        :param value: Value of terminal.
+        :param path: The path of the folder to save.
+        :return:
+        """
         df: DataFrame = df.loc[df['terminal'] == value]
-        year_distribution = df.groupby('year')
-        monthly_distribution = df.groupby('month')
-        for year in year_distribution:
-            for month in monthly_distribution:
-                month[1].to_excel(f"{path}/{year[0]}.{month[0]:02d} auto_tracking.xlsx", index=False)
+        group_columns: DataFrameGroupBy = df.groupby(['enforce_auto_tracking', 'year', 'month'])
+        column: Tuple[Tuple, DataFrame]
+        for column in group_columns:
+            if not column[0][0]:
+                column[1]['is_auto_tracking'] = False
+                column[1]['is_auto_tracking'] = None
+            column[1].to_excel(f"{path}/{column[0][1]}.{column[0][2]:02d} auto_tracking.xlsx", index=False)
 
     def main(self) -> None:
         """
         The main function where we read the Excel file and write the file to json.
+        :return:
         """
         df: DataFrame = pd.read_excel(self.input_file_path)
         self.write_rows_by_terminal(df, "НУТЭП", f"{os.environ['XL_IDP_PATH_IMPORT']}/lines_nutep/flat_import_nutep")
