bb907f51d908a10a99edcd865871c3b29f011790
timur <timurka.zavyalov@mail.ru>
2023-10-06 10:27:40 +0300

    git init

--- scripts/main.py
+++ scripts/main.py
@@ -11,23 +11,26 @@ class AutoTracking(object):
         self.input_file_path: str = input_file_path
 
     @staticmethod
-    def write_rows_by_terminal(df: DataFrame, direction: str, terminal: str, path: str) -> None:
+    def write_rows_by_terminal(df: DataFrame, direction: str, terminals: list, path: str) -> None:
         """
         We group by 'enforce_auto_tracking', 'year', 'month' and write it to a separate xlsx file.
         :param df: DataFrame.
         :param direction: Value of direction.
-        :param terminal: Value of terminal.
+        :param terminals: List of value of terminal.
         :param path: The path of the folder to save.
         :return:
         """
-        df: DataFrame = df.loc[(df['terminal'] == terminal) & (df['direction'] == direction)]
-        group_columns: DataFrameGroupBy = df.groupby(['enforce_auto_tracking', 'original_file_name'])
-        column: Tuple[Tuple, DataFrame]
-        for column in group_columns:
-            if not column[0][0]:
-                column[1]['is_auto_tracking'] = False
-                column[1]['is_auto_tracking_ok'] = None
-            column[1].to_excel(f"{path}/{column[0][1].replace('.csv', '')}", index=False)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        for terminal in terminals:
+            df: DataFrame = df.loc[(df['terminal'] == terminal) & (df['direction'] == direction)]
+            group_columns: DataFrameGroupBy = df.groupby(['enforce_auto_tracking', 'original_file_name'])
+            column: Tuple[Tuple, DataFrame]
+            for column in group_columns:
+                if not column[0][0]:
+                    column[1]['is_auto_tracking'] = False
+                    column[1]['is_auto_tracking_ok'] = None
+                column[1].to_excel(f"{path}/{column[0][1].replace('.csv', '')}", index=False)
 
     @staticmethod
     def change_types_in_columns(df: DataFrame) -> None:
@@ -47,16 +50,16 @@ class AutoTracking(object):
         """
         df: DataFrame = pd.read_excel(self.input_file_path)
         self.change_types_in_columns(df)
-        self.write_rows_by_terminal(df, "import", "НУТЭП", f"{os.environ['XL_IDP_PATH_IMPORT']}/"
-                                                           f"lines_nutep/flat_import_nutep_tracking_update")
-        self.write_rows_by_terminal(df, "import", "ВСК", f"{os.environ['XL_IDP_PATH_VSK_IMPORT']}/"
-                                                         f"flat_import_vsk_tracking_update")
-        self.write_rows_by_terminal(df, "export", "НУТЭП", f"{os.environ['XL_IDP_PATH_EXPORT']}/"
-                                                           f"lines_nutep/flat_export_nutep_tracking_update")
-        self.write_rows_by_terminal(df, "export", "ВСК", f"{os.environ['XL_IDP_PATH_VSK_EXPORT']}/"
-                                                         f"flat_export_vsk_tracking_update")
-        self.write_rows_by_terminal(df, "export", "ПЛП", f"{os.environ['XL_IDP_PATH_NW_EXPORT']}/"
-                                                         f"flat_export_nw_tracking_update")
+        self.write_rows_by_terminal(df, "import", ["НУТЭП"], f"{os.environ['XL_IDP_PATH_IMPORT']}/"
+                                                             f"lines_nutep/flat_import_nutep_tracking_update")
+        self.write_rows_by_terminal(df, "import", ["ВСК"], f"{os.environ['XL_IDP_PATH_VSK_IMPORT']}/"
+                                                           f"flat_import_vsk_tracking_update")
+        self.write_rows_by_terminal(df, "export", ["НУТЭП"], f"{os.environ['XL_IDP_PATH_EXPORT']}/"
+                                                             f"lines_nutep/flat_export_nutep_tracking_update")
+        self.write_rows_by_terminal(df, "export", ["ВСК"], f"{os.environ['XL_IDP_PATH_VSK_EXPORT']}/"
+                                                           f"flat_export_vsk_tracking_update")
+        self.write_rows_by_terminal(df, "export", ["ПКТ", "УЛКТ", "ПЛП"], f"{os.environ['XL_IDP_PATH_NW_EXPORT']}/"
+                                                                          f"flat_export_nw_tracking_update")
 
 
 if __name__ == "__main__":
